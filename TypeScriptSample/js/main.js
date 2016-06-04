5591265012a8a7763ae3b1e0a25bb07cd942faa4
elvisxia <xiaxucheng@126.com>
2016-06-04 11:51:40 +0800

    Add project files.

---
+++ TypeScriptSample/js/main.js
@@ -0,0 +1,5 @@
+function buildName(firstName, lastName) {
+    return firstName + " " + lastName;
+}
+var result3 = buildName("Bob", "Adams");
+//# sourceMappingURL=main.js.map
---
+++ TypeScriptSample/js/main.ts
@@ -0,0 +1,5 @@
+﻿function buildName(firstName: string, lastName: string) {
+    return firstName + " " + lastName;
+}
+
+let result3 = buildName("Bob", "Adams");
